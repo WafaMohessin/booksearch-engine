6ef98665f83b1c5aa58295feadaaf0b6c29c8e77
Wafa Mohessin <wafamohessin@gmail.com>
2022-01-21 20:48:10 -0500

    server.js

---
+++ booksearch-engine/server.js
@@ -0,0 +1,34 @@
+const exports = require('express');
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs
+})
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  // app.use(routes);
+  
+  db.once('open', async () => {
+  
+    await server.start()
+    server.applyMiddleware({ app })
+    
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+      console.log(`GraphQL server available at http://localhost:${PORT}${server.graphqlPath}`)
+    });
+  });
+  
